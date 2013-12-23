452af81baed9c9e57982ab26bcf94651bdbd828e
jonneale <jonneale@gmail.com>
2013-12-23 10:26:42 +0000

    initial commit

---
+++ test/crackers_service/core_test.clj
@@ -0,0 +1,7 @@
+(ns crackers-service.core-test
+  (:require [clojure.test :refer :all]
+            [crackers-service.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
