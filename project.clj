715ac9ddc318c51679cb6283b67203a55bd4b3c7
Jon Neale <jon.neale@USW-00589.local>
2016-12-23 10:34:47 +0000

    Updated clojure version

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]
-                 [ring "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring "1.5.0"]
                  [org.clojure/data.json "0.2.3"]
                  [org.clojure/data.csv "0.1.2"]]
   :plugins [[lein-ring "0.8.5"]]
